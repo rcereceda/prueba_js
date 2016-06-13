68a21203c519d0daa2bef5c6bbf73f17492408bb
Rodrigo Cereceda <rcereced@gmail.com>
2016-06-13 19:09:03 -0300

    Pregunta 1

---
+++ p1/main.js
@@ -0,0 +1,6 @@
+var alumno1 = {nombre:"Matías", edad:5}
+var alumno2 = alumno1
+alumno2.nombre = "Sebastián"
+
+console.log("Alumno 1: " + alumno1.nombre);
+console.log("Alumno 2: " + alumno2.nombre);
