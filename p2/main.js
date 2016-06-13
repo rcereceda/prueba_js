60db465351ec605cbad06f41a19e3abe81e0928a
Rodrigo Cereceda <rcereced@gmail.com>
2016-06-13 19:18:29 -0300

    Pregunta 2

---
+++ p2/main.js
@@ -0,0 +1,16 @@
+var user1 = {
+	nombre: "Rodrigo",
+	edad: 33,
+	arr: [1,2,3]
+};
+	
+function copiarObjeto(obj) {
+	return (JSON.parse(JSON.stringify(obj)));
+}
+
+var user2 = copiarObjeto(user1);
+
+user2.nombre = "Jorge";
+
+console.log("Rodrigo: " + user1.arr);
+console.log("Jorge: " + user2.arr);
