a1b17ef3529a06a990f2ce99e0259f7f95e061c6
Rodrigo Cereceda <rcereced@gmail.com>
2016-06-13 21:22:38 -0300

    Pregunta 3 actualizada

--- p3/main.js
+++ p3/main.js
@@ -1,15 +1,17 @@
 function Alumno(nombre, notas) {
-	this.nombre = nombre;
-	this.notas = notas;
+	this.nombre = typeof(nombre) == "string" ? nombre : "Juan";
+	this.notas = Array.isArray(notas) ? notas : [0, 0, 0];
 
 	this.promedio = function() {
 		var suma = 0;
+		var n = 0;
 		for (var i = 0; i < this.notas.length; i++) {
 			if (!(isNaN(this.notas[i]))) {
 				suma += parseInt(this.notas[i]);
+				n++;
 			}
 		}
-		return (suma/this.notas.length);
+		return n > 0 ? (suma/n) : 0;
 	}
 }
 
